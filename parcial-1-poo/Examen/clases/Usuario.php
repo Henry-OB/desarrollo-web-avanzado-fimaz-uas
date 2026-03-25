6703d869683d5dc08bdc36b777af974f3ac627f6
Henry-OB <hosuna2345@gmail.com>
2026-03-24 17:25:14 -0700

    Create Usuario.php

---
+++ parcial-1-poo/Examen/clases/Usuario.php
@@ -0,0 +1,26 @@
+<?php
+
+class Usuario {
+    
+    protected $nombre;
+    protected $correo;
+
+    public function __construct($nombre, $correo) {
+        
+        if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("El formato del correo electrónico no es válido.");
+        }
+        
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getCorreo() {
+        return $this->correo;
+    }
+}
+?>
